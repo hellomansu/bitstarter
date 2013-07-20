cf8efa4b1c4175338a9e52b0fcec6af855e0aac8
Suman Polavarapu <p_sumankumar@yahoo.com>
2013-07-20 04:56:26 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
-
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var buf = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
